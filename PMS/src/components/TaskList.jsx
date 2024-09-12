74707173ca5268b72d22e39b26fe795fccaf6059
umardraz2004 <umardraz6965@gmail.com>
2024-09-12 22:59:25 +0500

    Task list styling added

--- PMS/src/components/TaskList.jsx
+++ PMS/src/components/TaskList.jsx
@@ -1,6 +1,11 @@
-    const TaskList = () => {
+const TaskList = () => {
   return (
-    <div>TaskList</div>
+    <ol className="mt-5 bg-gray-100 px-5 py-7">
+        <li className="flex justify-between py-2 font-inter font-medium">
+            <p>Practice 1, practice 2, practice 3</p>
+            <button>Clear</button>
+        </li>
+    </ol>
   )
 }
 
